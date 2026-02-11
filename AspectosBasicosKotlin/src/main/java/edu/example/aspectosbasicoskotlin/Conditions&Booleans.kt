fe777aa039f75e965afb1e215a84f0d3b568da6e
erodriguez84x <erodriguez84@alumnos.uaq.mx>
2026-02-11 08:52:32 -0600

    Conditions&Booleans update

--- AspectosBasicosKotlin/src/main/java/edu/example/aspectosbasicoskotlin/Conditions&Booleans.kt
+++ AspectosBasicosKotlin/src/main/java/edu/example/aspectosbasicoskotlin/Conditions&Booleans.kt
@@ -1,2 +1,34 @@
 package edu.example.aspectosbasicoskotlin
 
+fun main() {
+    val numberOfFish = 50
+    val numberOfPlants = 23
+
+    // comparacion basica
+    if (numberOfFish > numberOfPlants) {
+        println("Good ratio!")
+    } else {
+        println("Unhealthy ratio")
+    }
+
+    // rango de 1 a 100
+    if (numberOfFish in 1..100) {
+        println(numberOfFish)
+    }
+
+    // if - else if - else
+    if (numberOfFish == 0) {
+        println("Empty tank")
+    } else if (numberOfFish < 40) {
+        println("Got fish!")
+    } else {
+        println("That's a lot of fish!")
+    }
+
+    // estructura when (mas limpia)
+    when (numberOfFish) {
+        0  -> println("Empty tank")
+        in 1..39 -> println("Got fish!")
+        else -> println("That's a lot of fish!")
+    }
+}
