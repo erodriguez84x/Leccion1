51ef0836d6bd2d9f51c96c277a9de97340606636
erodriguez84x <erodriguez84@alumnos.uaq.mx>
2026-02-17 20:05:25 -0600

    Hello

--- Leccion2_Funciones/src/main/java/edu/example/leccion2_funciones/Hello.kt
+++ Leccion2_Funciones/src/main/java/edu/example/leccion2_funciones/Hello.kt
@@ -1,5 +1,22 @@
 package edu.example.leccion2_funciones
 
 fun main(args: Array<String>) {
+
+    //---1
     println("Hello, world!")
+
+    //---2
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    //---3
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
+
+    //---4
+    val temperature2 = 10
+    val message = "The water temperature is ${ if (temperature2 > 50) "too warm" else "OK" }."
+    println(message)
+
 }
