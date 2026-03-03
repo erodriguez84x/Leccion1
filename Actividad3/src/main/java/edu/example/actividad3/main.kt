938895988f2ea60eea0370cd47cfa34007d45de1
erodriguez84x <erodriguez84@alumnos.uaq.mx>
2026-03-03 03:03:36 -0600

    Actividad3

--- Actividad3/src/main/java/edu/example/actividad3/main.kt
+++ Actividad3/src/main/java/edu/example/actividad3/main.kt
@@ -3,25 +3,25 @@ package edu.example.actividad3
 fun main() {
     println("--- Iniciando Verificacion -----------")
 
-    // Paso 1: Instanciación
+    //instanciacion
     val libroClasico = Libro("Don Quijote de la Mancha", 1605)
     val libroModerno = Libro("Cien años de soledad", 1967)
 
-    // Paso 2: Prueba de Herencia y Abstracción
+    //herencia y abstracción
     println("\n--- Prueba de Herencia y Abstraccion -----------")
     println("Título: ${libroClasico.titulo}")
     println("Anio de publicacion: ${libroClasico.anioPublicacion}")
 
-    // Paso 3: Prueba de Interfaz
+    //interfaz
     println("\n--- Prueba de Interfaz -----------")
     libroClasico.prestar()
 
-    // Paso 4: Prueba de Función de Extensión
+    //funcion de extension
     println("\n--- Prueba de Funcion de Extension -----------")
     println("¿El libro '${libroClasico.titulo}' es antiguo? ${libroClasico.esAntiguo()}")
     println("¿El libro '${libroModerno.titulo}' es antiguo? ${libroModerno.esAntiguo()}")
 
-    // Paso 5: Prueba del Companion Object
+    // Companion Object
     println("\n--- Prueba del Companion Object -----------")
     println("Constante TIPO_RECURSO: ${Libro.TIPO_RECURSO}")
 }
